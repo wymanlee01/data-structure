16d28ebff63e91e2d504903811dac7f8f0d2cf8c
liweiwen <zflww.123>
2021-10-18 14:51:20 +0800

    Rebase2初始化

---
+++ src/main/java/com/wyman/datastructor/rebase2/Rebase2.java
@@ -0,0 +1,11 @@
+package com.wyman.datastructor.rebase2;
+
+/**
+ * @author liweiwen
+ * @since 2021-10-18
+ */
+public class Rebase2 {
+    public static void main(String[] args) {
+        System.out.println("rebase2初始化");
+    }
+}
