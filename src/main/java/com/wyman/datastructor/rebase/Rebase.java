d45cc7d2589275a896c097dc80cae9e3553136f0
liweiwen <zflww.123>
2021-10-18 14:25:36 +0800

    第一次rebase

---
+++ src/main/java/com/wyman/datastructor/rebase/Rebase.java
@@ -0,0 +1,11 @@
+package com.wyman.datastructor.rebase;
+
+/**
+ * @author liweiwen
+ * @since 2021-10-18
+ */
+public class Rebase {
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+}
