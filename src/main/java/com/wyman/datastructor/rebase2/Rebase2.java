dfdcd8bab53dbf6251ed8780ed1471024021c94a
liweiwen <zflww.123>
2021-10-18 14:54:08 +0800

    Rebase2第一次

--- src/main/java/com/wyman/datastructor/rebase2/Rebase2.java
+++ src/main/java/com/wyman/datastructor/rebase2/Rebase2.java
@@ -7,5 +7,6 @@ package com.wyman.datastructor.rebase2;
 public class Rebase2 {
     public static void main(String[] args) {
         System.out.println("rebase2初始化");
+        System.out.println("rebase21第一次");
     }
 }
