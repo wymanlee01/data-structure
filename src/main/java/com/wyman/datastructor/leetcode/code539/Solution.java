4a8230a3cb456d6bfe8e3e67075112d4b607b6ab
liweiwen <1715124622@qq.com>
2022-01-19 20:26:24 +0800

    最小时间差

---
+++ src/main/java/com/wyman/datastructor/leetcode/code539/Solution.java
@@ -0,0 +1,34 @@
+package com.wyman.datastructor.leetcode.code539;
+
+import java.util.*;
+
+/**
+ * 539. 最小时间差
+ */
+class Solution {
+    public int findMinDifference(List<String> timePoints) {
+        if (timePoints.size() > 1440) {
+            return 0;
+        }
+        List<Integer> minutePoints = new ArrayList<>();
+        for (String timePoint : timePoints) {
+            String[] split = timePoint.split(":");
+            minutePoints.add(Integer.parseInt(split[0]) * 60 + Integer.parseInt(split[1]));
+        }
+        Collections.sort(minutePoints);
+        int min = Integer.MAX_VALUE;
+        int size = minutePoints.size();
+        for (int i = 0; i < size - 1; i++) {
+            min = Math.min(min, minutePoints.get(i + 1) - minutePoints.get(i));
+        }
+        // 隔天情况
+        int result = Math.min(min, minutePoints.get(0) + 1440 - minutePoints.get(size - 1));
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String[] arr = new String[]{"23:59","00:00"};
+        int minDifference = new Solution().findMinDifference(Arrays.asList(arr));
+        System.out.println(minDifference);
+    }
+}
